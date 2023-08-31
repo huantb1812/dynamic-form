1d1d734f79b41047f1f9549f62f13ea2ca6a2b19
Nguyen Van Huan <huantb1812@gmail.com>
2023-08-31 23:21:09 +0800

    update drag drop container

--- src/app/shared/json-form-viewer/design-layout/design-layout.component.ts
+++ src/app/shared/json-form-viewer/design-layout/design-layout.component.ts
@@ -1,12 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+import { NgFor } from '@angular/common';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+  copyArrayItem,
+} from '@angular/cdk/drag-drop';
+import { Field } from '../../models/field';
+import { FIELDS, LAYOUTS } from '../../models';
 @Component({
   selector: 'app-design-layout',
   templateUrl: './design-layout.component.html',
   styleUrls: ['./design-layout.component.css'],
 })
 export class DesignLayoutComponent implements OnInit {
+  doneList: Field[] = [];
+
+  @Input('connectedTo')
+  connectedTo: string[];
   constructor() {}
+
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      // copyArrayItem(
+      //   event.previousContainer.data,
+      //   event.container.data,
+      //   event.previousIndex,
+      //   event.currentIndex
+      // );
+
+      transferArrayItem([event.item.data],
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+    // if (event.previousContainer.id === event.container.id) {
+    //   // moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    // } else {
+    //   copyArrayItem(
+    //     event.previousContainer.data,
+    //     event.container.data,
+    //     event.previousIndex,
+    //     event.currentIndex
+    //   );
+    // }
+  }
 
   ngOnInit() {}
 }
